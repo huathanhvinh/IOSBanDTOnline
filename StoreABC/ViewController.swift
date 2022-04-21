1926278d1ad4edf70f964308b09064e84afa43ec
Thanh Vinh <vinhthanh@MacBook-Pro-cua-Thanh.local>
2022-04-21 14:14:24 +0700

    Create project

---
+++ StoreABC/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  StoreABC
+//
+//  Created by CNTT on 4/20/22.
+//  Copyright © 2022 fit.tdc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
